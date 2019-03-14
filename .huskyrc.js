871210f3b456153af080e06011669e211712905f
Mathias <mathias@kandelborg.dk>
2019-03-14 15:33:34 +0100

    build(husky): run tslint on pre-commit

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,10 @@ const tasks = (arr) => arr.join(' && ')
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['lint-staged']),
+    'pre-commit': tasks([
+      'tslint -p tsconfig.json --config tslint.json',
+      'lint-staged'
+    ]),
     'commit-msg': tasks(['commitlint -E HUSKY_GIT_PARAMS'])
   }
 }
